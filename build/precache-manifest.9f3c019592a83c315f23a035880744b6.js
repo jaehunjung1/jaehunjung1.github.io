d3cab23d30d23af0fd93e43265da79907f679dcc
Jaehun Jung <sharkmir1@snu.ac.kr>
2022-10-27 14:46:21 -0700

    changes

---
+++ build/precache-manifest.9f3c019592a83c315f23a035880744b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffe41bfe93dabe2b6a13",
+    "url": "/static/css/main.d7313e6e.chunk.css"
+  },
+  {
+    "revision": "ffe41bfe93dabe2b6a13",
+    "url": "/static/js/main.a3008b41.chunk.js"
+  },
+  {
+    "revision": "e9bf061c374b598820bc",
+    "url": "/static/js/runtime~main.1e435022.js"
+  },
+  {
+    "revision": "74d6755879ff78ddfb31",
+    "url": "/static/js/2.e300c7b3.chunk.js"
+  },
+  {
+    "revision": "8bcc0fd0feec533cc418",
+    "url": "/static/js/3.568a3ed3.chunk.js"
+  },
+  {
+    "revision": "5141f2d25aad833447665868a5402b62",
+    "url": "/static/media/profilePic.5141f2d2.JPG"
+  },
+  {
+    "revision": "0ced9720bcedcc997e1dc8bbe43e062f",
+    "url": "/static/media/tgap.0ced9720.png"
+  },
+  {
+    "revision": "d3e9736a2cb2d27b451445685c3ab73f",
+    "url": "/index.html"
+  }
+];
